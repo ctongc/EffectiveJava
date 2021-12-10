2e02f262273e54ebce2fb27002a610bd9af1bbec
Chao Tong <tongchao0428@hotmail.com>
2021-12-10 23:36:54 +0800

    Item 3

---
+++ src/Item3Singleton.java
@@ -0,0 +1,37 @@
+/**
+ * Item 3
+ * Enforce the singleton property with a private constructor or an enum type
+ */
+class SingletonWithPublicFinalField {
+    public static final SingletonWithPublicFinalField INSTANCE = new SingletonWithPublicFinalField();
+
+    private SingletonWithPublicFinalField() {
+    }
+
+    public void doSomething() {
+    }
+}
+
+class SingletonWithStaticFactory {
+    private static final SingletonWithStaticFactory INSTANCE = new SingletonWithStaticFactory();
+
+    private SingletonWithStaticFactory() {
+    }
+
+    public static SingletonWithStaticFactory getInstance() {
+        return INSTANCE;
+    }
+
+    public void doSomething() {
+    }
+}
+
+enum SingletonWithEnum {
+    INSTANCE;
+
+    public void doSomething() {
+    }
+}
+
+public class Item3Singleton {
+}
